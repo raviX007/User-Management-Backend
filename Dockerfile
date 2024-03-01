ec694638d8b0a2fbbb1c63372b848377a96cdf89
raviX007 <91694681+raviX007@users.noreply.github.com>
2024-03-02 04:06:24 +0530

    Second Commit

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
-FROM node:latest
-
-WORKDIR /app
+FROM node:alpine
+RUN mkdir -p /project/app
+WORKDIR /project
 
 COPY package.json ./
 COPY server.js ./
 
 RUN npm install 
 
-COPY . .
+COPY ./app/ ./app/
 
-EXPOSE 5000
 
-CMD ["npm","start"]
+
+# EXPOSE 8080
+
+CMD ["node","server.js"]
--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,9 @@
 const dbName = process.env.MONGODB_DEFAULT_DB;
 const dbHost = process.env.MONGODB_HOST;
-
+const mongoUser=process.env.MONGO_USERNAME
+const mongoPassword=process.env.MONGO_PASSWORD
+const encodedPassword = encodeURIComponent(mongoPassword);
 const dbConfig = {
-  url: `mongodb://${dbHost}/${dbName}`,
+  url: `mongodb+srv://${mongoUser}:${encodedPassword}@${dbHost}/${dbName}`,
 };
 export default dbConfig;
