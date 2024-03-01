960ea9b7c01a37ed482e0d654608d115568f86e7
raviX007 <91694681+raviX007@users.noreply.github.com>
2024-03-02 04:37:51 +0530

    Fourth Commit

--- server.js
+++ server.js
@@ -46,8 +46,4 @@ routes(app);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
-  console.log("Default Db:", process.env.MONGODB_DEFAULT_DB);
-  console.log("localhost:",process.env.MONGODB_HOST);
-  console.log("gmail",process.env.GMAIL_USERNAME);
-  console.log("gmail_app_pswd",process.env.GMAIL_APP_PASSWORD);
 });
